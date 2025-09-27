1666cf8273db3bc440c5b15346d2ebf5ec6361ef
Raj84 <patoliyaraj2222@gmail.com>
2025-09-28 00:01:19 +0530

    add the course update functinalty in eduvers for admins

--- src/Controller/CourseController.js
+++ src/Controller/CourseController.js
@@ -124,8 +124,137 @@ const DeleteCourse = async (req, res) => {
   }
 };
 
+const getcourseById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    if (!id) {
+      return res.status(400).json({
+        message: "Course ID is required",
+        success: false,
+      });
+    }
+
+    const course = await Course.findById(id);
+    if (!course) {
+      return res.status(404).json({
+        message: "Course Not Found",
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      message: "Course Retrieved Successfully",
+      success: true,
+      data: course,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal Server Error, Please Try Again",
+      success: false,
+    });
+  }
+};
+
+const UpdateCourse = async (req, res) => {
+  if (!req.body || Object.keys(req.body).length === 0) {
+    return res.status(400).json({
+      message: "Request body is empty or not properly parsed",
+      success: false,
+    });
+  }
+
+  try {
+    const { title, image, description, category, videoUrl, tags } = req.body;
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(404).json({
+        message: "Occurs Some Issues Please Try Agani",
+        success: false,
+      });
+    }
+
+    const course = await Course.findById(id);
+
+    if (!course) {
+      return res.status(404).json({
+        message: "Course not found",
+        success: false,
+      });
+    }
+
+    if (!title.length > 0) {
+      return res.status(400).json({
+        message: "Title is required",
+        success: false,
+      });
+    }
+    if (!image.length > 0) {
+      return res.status(400).json({
+        message: "Image is required",
+        success: false,
+      });
+    }
+    if (!description.length > 0) {
+      return res.status(400).json({
+        message: "Description is required",
+        success: false,
+      });
+    }
+    if (!category.length > 0) {
+      return res.status(400).json({
+        message: "Category is required",
+        success: false,
+      });
+    }
+    if (!videoUrl.length > 0) {
+      return res.status(400).json({
+        message: "Video URL is required",
+        success: false,
+      });
+    }
+
+    const updatedCourse = await Course.findByIdAndUpdate(
+      id,
+      {
+        $set: {
+          ...(title && { title }),
+          ...(image && { image }),
+          ...(description && { description }),
+          ...(category && { category }),
+          ...(videoUrl && { videoUrl }),
+          ...(tags && { tags }),
+        },
+      },
+      { new: true }
+    );
+
+    if (!updatedCourse) {
+      return res.status(400).json({
+        message: "Failed to update course, please try again",
+        success: false,
+      });
+    }
+
+    return res.status(200).json({
+      message: "Course updated successfully",
+      success: true,
+      data: updatedCourse,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal Server Error, Please Try Again",
+      success: false,
+    });
+  }
+};
+
 module.exports = {
   CreateCourse,
   allCourses,
   DeleteCourse,
+  getcourseById,
+  UpdateCourse
 };
--- src/Routers/CourseRoutes.js
+++ src/Routers/CourseRoutes.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
-const { CreateCourse, allCourses, DeleteCourse } = require("../Controller/CourseController");
+const { CreateCourse, allCourses, DeleteCourse, getcourseById, UpdateCourse } = require("../Controller/CourseController");
 
 router.post("/create", CreateCourse);
 router.get("/all", allCourses);
 router.delete("/delete", DeleteCourse);
+router.get("/get/:id", getcourseById);
+router.patch("/update/:id", UpdateCourse);
 
 module.exports = router;
