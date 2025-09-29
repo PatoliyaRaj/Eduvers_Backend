5c66f58d540e5ff00be612acf0912a884249cf51
Raj84 <patoliyaraj2222@gmail.com>
2025-09-29 16:42:54 +0530

    add atlas for deployment

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes (MUST come after middleware)
+app.use('/', (req, res) => {
+  res.send('Welcome to the EduVers API');
+});
 app.use("/User", signupRoutes);
 app.use("/Course", CourseRoutes);
 app.use("/Contact", ContactUsRoutes);
